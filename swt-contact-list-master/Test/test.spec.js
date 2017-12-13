0fdf9cff4d447684f90feee3be1cfd26dacc0c2b
jakkrawut022 <jakkrawut022@gmail.com>
2017-12-13 19:16:28 +0700

    Update contracts

--- swt-contact-list-master/Test/test.spec.js
+++ swt-contact-list-master/Test/test.spec.js
@@ -16,8 +16,32 @@ describe("GET /contacts", () => {
                 expect(contact).toHaveProperty("url")
                 expect(contact).toHaveProperty("notes")
                 done()
+            })
+    })
+})
 
+describe("POST /contacts", () => {
+    it("เพิ่มรายชื่อเข้ารายการ", (done) => {
+        let sendContact = {
+            name: 'jesica',
+            email: 'jesica@gmail.com',
+            phone: '0877771234',
+            url: 'www.google.com',
+            notes: 'ทำได้แค่นี้'
+        }
 
+        request(app).post("/contacts")
+            .send(sendContact)
+            .expect(201)
+            .then((res) => {
+                let reciveContact = res.body
+                expect(reciveContact).toHaveProperty("id", 12)
+                expect(reciveContact).toHaveProperty("name", sendContact.name)
+                expect(reciveContact).toHaveProperty("email", sendContact.email)
+                expect(reciveContact).toHaveProperty("phone", sendContact.phone)
+                expect(reciveContact).toHaveProperty("url", sendContact.url)
+                expect(reciveContact).toHaveProperty("notes", sendContact.notes)
+                done()
             })
     })
 })
